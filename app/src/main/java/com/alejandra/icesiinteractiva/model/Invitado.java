04a6bc80418beddd1e4ade8f76b7ffdc3a86609c
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-04-09 11:38:20 -0500

    Modelo1

--- app/src/main/java/com/alejandra/icesiinteractiva/model/Invitado.java
+++ app/src/main/java/com/alejandra/icesiinteractiva/model/Invitado.java
@@ -1,4 +1,31 @@
 package com.alejandra.icesiinteractiva.model;
 
 public class Invitado {
+
+    private String email;
+    private String nickname;
+
+    public Invitado() {
+    }
+
+    public Invitado(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
