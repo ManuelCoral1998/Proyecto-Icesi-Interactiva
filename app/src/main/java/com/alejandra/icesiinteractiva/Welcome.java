eb26a4ab0fa0decaf206e519930c9b26ffefe0a6
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-04-09 11:41:39 -0500

    CambiosGenerales

--- app/src/main/java/com/alejandra/icesiinteractiva/Welcome.java
+++ app/src/main/java/com/alejandra/icesiinteractiva/Welcome.java
@@ -3,11 +3,11 @@ package com.alejandra.icesiinteractiva;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class login extends AppCompatActivity {
+public class Welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_welcome);
     }
 }
