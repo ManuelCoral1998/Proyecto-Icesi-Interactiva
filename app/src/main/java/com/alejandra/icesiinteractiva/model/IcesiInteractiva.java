777127101d48c8a42af84c1e860f78bba333625e
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-04-09 11:21:37 -0500

    Modelo1

---
+++ app/src/main/java/com/alejandra/icesiinteractiva/model/IcesiInteractiva.java
@@ -0,0 +1,10 @@
+package com.alejandra.icesiinteractiva.model;
+
+import java.util.ArrayList;
+
+public class IcesiInteractiva {
+
+    private Invitado invitado;
+    private ArrayList<Proyecto> proyectos;
+
+}
