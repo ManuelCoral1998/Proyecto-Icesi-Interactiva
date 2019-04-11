13bac1c4c699350bce70691c3508c3435a38a508
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-04-11 13:16:54 -0500

    Espacios

--- app/src/main/java/com/alejandra/icesiinteractiva/Welcome.java
+++ app/src/main/java/com/alejandra/icesiinteractiva/Welcome.java
@@ -1,13 +1,35 @@
 package com.alejandra.icesiinteractiva;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Welcome extends AppCompatActivity {
+
+    private TextView tv_welcome;
+    private Button btn_continuar_welcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        tv_welcome = findViewById(R.id.tv_welcome);
+
+        tv_welcome.getBackground().setAlpha(60);
+
+        btn_continuar_welcome = findViewById(R.id.btn_continuar_welcome);
+
+        btn_continuar_welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ProjectList.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
