6c55c6df070217cc2a6e125d62325f29c9c6d906
ManuelCoral1998 <43486285+ManuelCoral1998@users.noreply.github.com>
2019-04-15 10:09:55 -0500

    Splash

--- app/src/main/java/com/alejandra/icesiinteractiva/SplashScreen.java
+++ app/src/main/java/com/alejandra/icesiinteractiva/SplashScreen.java
@@ -6,19 +6,33 @@ import android.content.pm.ActivityInfo;
 import android.os.Handler;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 public class SplashScreen extends Activity {
 
-    private final int DURACION = 1000;
+    private final int DURACION = 2000;
+    private ImageView imagenSplanScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_splash_screen);
+
+        imagenSplanScreen = findViewById(R.id.im_splash_screen);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_splash);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.setRepeatCount(Animation.INFINITE);
+        animation.setDuration(DURACION);
+
+        imagenSplanScreen.startAnimation(animation);
 
         new Handler().postDelayed(new Runnable() {
             @Override
